b05e7c2b7c8b7fd2057a9d9b6e251b445a734ea6
Supasan Komonlit <supasan_224@hotmail.com>
2019-04-01 17:41:55 +0700

    get loop to continue run can lear pattern

--- fan_in.go
+++ fan_in.go
@@ -4,13 +4,17 @@ import "fmt"
 import "time"
 
 func worker1( c chan int ){
-    time.Sleep( 1 * time.Second )
-    c <- 10
+    for{
+        time.Sleep( 1 * time.Second )
+        c <- 10
+    }
 }
 
 func worker2( c chan int ){
-    time.Sleep( 4 * time.Second )
-    c <- 20
+    for{
+        time.Sleep( 4 * time.Second )
+        c <- 20
+    }
 }
 
 func main(){
@@ -18,10 +22,13 @@ func main(){
     c2 := make( chan int )
     go worker1( c1 )
     go worker2( c2 )
-    select { // command to make we can wait data from to many channal
-        case n := <- c1:
-            fmt.Println( n )
-        case n := <- c2:
-            fmt.Println( n )
+    // select coammand will wait untill data have come to varaible can check
+    for{
+        select { // command to make we can wait data from to many channal
+            case n := <- c1:
+                fmt.Println( n )
+            case n := <- c2:
+                fmt.Println( n )
+        }
     }
 }
